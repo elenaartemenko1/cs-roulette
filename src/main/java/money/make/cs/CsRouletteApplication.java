4686a489e6d3701bff2cb7a99fa121008eff2999
elenaartemenko1 <dubeyelena97@gmail.com>
2022-09-27 20:13:38 +0300

    first commit

---
+++ src/main/java/money/make/cs/CsRouletteApplication.java
@@ -0,0 +1,13 @@
+package money.make.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsRouletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/money/make/cs/CsRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package money.make.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
